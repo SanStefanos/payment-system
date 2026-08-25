f4a39b5377c05d395ba049674e4cf9b3accfcc98
Alex <alex-email@mail.com>
2026-08-25 15:52:31 +0300

    Implement progressive commission
    Add payment analytics

--- src/commission.js
+++ src/commission.js
@@ -3,4 +3,21 @@ function calculateCommission(amount, rate) {
     return amount * rate;
 }
 
-module.exports = { calculateCommission };
+function calculateProgressiveCommission(amount, rate) {
+    if (amount < 100) {
+        return amount * rate;
+    }
+
+    if (amount < 1000) {
+        return (100 * rate) + ((amount - 100) * rate * 0.8);
+    }
+
+    return (100 * rate) +
+           (900 * rate * 0.8) +
+           ((amount - 1000) * rate * 0.5);
+}
+
+module.exports = {
+    calculateCommission,
+    calculateProgressiveCommission
+};
